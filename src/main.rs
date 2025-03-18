d9d1a597f455a502457ff8a398a584ecab1e64af
masamallow <77992722+masamallow@users.noreply.github.com>
2025-03-18 22:02:19 +0900

    Define argument analysis with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use anyhow::Result;
+
+/// CLI tool for creating ZIP files that don't get garbled on Windows
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Target directory you want to ZIP compress
+    target_dir: String,
+
+    /// Output file name (directory name.zip if omitted)
+    output_file: Option<String>,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    // Checking argument contents (for debugging)
+    println!("Target directory: {}", args.target_dir);
+    if let Some(out) = args.output_file.as_ref() {
+        println!("Output file name: {}", out);
+    } else {
+        println!("Output file name omitted");
+    }
+
+    Ok(())
 }
